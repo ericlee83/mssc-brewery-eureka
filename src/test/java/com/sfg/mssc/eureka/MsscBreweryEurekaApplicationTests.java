6bb6f3162a0f68292b66b7d01b64b296e3b76604
ericlee83 <eric-lee-83@hotmail.com>
2020-07-16 16:43:29 +1000

    init

---
+++ src/test/java/com/sfg/mssc/eureka/MsscBreweryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfg.mssc.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
